44a513f03c14e15a4fe1b2d3e446cef80438cbb8
Pavel <pkondrashkov@gmail.com>
2020-02-02 19:29:03 +0300

    Updated SPM to support swift 5

--- Package.swift
+++ Package.swift
@@ -1,22 +1,24 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
     name: "main",
     dependencies: [
-        .Package(url: "https://github.com/SwiftGL/OpenGL.git", majorVersion: 3),
-        .Package(url: "https://github.com/SwiftGL/Math.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftGL/Image.git", majorVersion: 2)
+        .package(url: "https://github.com/SwiftGL/OpenGL.git", from: "3.1.1"),
+        .package(url: "https://github.com/SwiftGL/Math.git", from: "3.0.0"),
+        .package(url: "https://github.com/SwiftGL/Image.git", from: "2.2.0")
     ]
 )
 
 #if os(Linux)
     package.dependencies.append(
-        Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFW3Linux.git", majorVersion: 1)
+        .package(url: "https://github.com/SwiftGL/CGLFW3Linux.git", from: "1.0.1")
         // If your distro renamed the library to "glfw" (no 3) use this instead:
-        // Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFWLinux.git", majorVersion: 1)
+        // package.Dependency.package(url: "https://github.com/SwiftGL/CGLFWLinux.git", majorVersion: 1)
     )
 #else
     package.dependencies.append(
-        Package.Dependency.Package(url: "https://github.com/SwiftGL/CGLFW3.git", majorVersion: 2)
+        .package(url: "https://github.com/pkondrashkov/CGLFW3.git", .branch("master"))
     )
 #endif
